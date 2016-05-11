bfef3992901525628612cbd962ed07c5c74bb899
Donald Sullon <alexsullonporras@hotmail.com>
2016-05-11 18:05:10 -0500

    Creación del proyecto

---
+++ DemoServicio/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoServicio.Controllers
+{
+    public class HomeController : Controller
+    {
+        FootballWS.InfoSoapTypeClient proxy = new FootballWS.InfoSoapTypeClient("InfoSoap");
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Teams()
+        {
+            var listado = proxy.Teams().ToList();
+            return View(listado);
+        }
+    }
+}
